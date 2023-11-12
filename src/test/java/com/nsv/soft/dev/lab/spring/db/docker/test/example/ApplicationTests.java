c69048a2978035662d4cf7e53e1a9c77562d5350
NosovArtem <nosovartem1992@gmail.com>
2023-11-12 14:29:31 +0100

    init

---
+++ src/test/java/com/nsv/soft/dev/lab/spring/db/docker/test/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsv.soft.dev.lab.spring.db.docker.test.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
